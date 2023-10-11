e5e55909b75d7415d77b86637f8030298e6c3d80
Quoc Toan <117724428+QuocToann@users.noreply.github.com>
2023-10-11 20:46:47 +0700

    Lab3_Task2_class Product.java

--- Lab3_Task2_class Product.java
+++ Lab3_Task2_class Product.java
@@ -8,7 +8,6 @@ public class Product implements Comparable<Product> {
 	String type;
 
 	public Product(String id, String name, double price, String type) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.price = price;
