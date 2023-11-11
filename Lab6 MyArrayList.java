296b6c92ab1c38f2ef7d42e2a50f32e129310a5f
Quoc Toan <117724428+QuocToann@users.noreply.github.com>
2023-11-11 16:34:05 +0700

    Lab6 MyArrayList.java

--- Lab6 MyArrayList.java
+++ Lab6 MyArrayList.java
@@ -178,4 +178,18 @@ public class MyArrayList<E> {
 		Arrays.sort(elements, 0, size, c);
 	}
 
+	public static void main(String[] args) {
+			MyArrayList<Integer> list = new MyArrayList<>();
+			for (int i = 0; i < 3; i++) {
+				list.add(i);
+				System.out.println(list.getSize());
+			}
+
+			MyArrayList<Integer> list1 = new MyArrayList<>();
+			System.out.println(list1.isEmpty());
+	
+			MyArrayList<Integer> list3 = new MyArrayList<>();
+			list.clear();
+			System.out.println(Arrays.toString(list.toArray()));
+		}
 }
